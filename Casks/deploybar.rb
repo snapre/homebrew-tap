63c7054522a2a5f60aa68e9d9e09a649a4587911
snapre <xiangwu619@gmail.com>
2026-05-22 19:43:02 +0800

    Add DeployBar cask

---
+++ Casks/deploybar.rb
@@ -0,0 +1,19 @@
+cask "deploybar" do
+  version "0.1.5"
+  sha256 "3099db1f3951f3ecb0100c01574dd9a970ecae7d6674d4b43cfab38c92271832"
+
+  url "https://github.com/snapre/DeployBar/releases/download/v#{version}/DeployBar-#{version}-macOS.zip"
+  name "DeployBar"
+  desc "Local-first macOS menu bar app for watching cloud deployment status"
+  homepage "https://github.com/snapre/DeployBar"
+
+  depends_on macos: ">= :sonoma"
+
+  app "DeployBar.app"
+
+  zap trash: [
+    "~/Library/Application Support/DeployBar",
+    "~/Library/Caches/com.deploybar.app",
+    "~/Library/Preferences/com.deploybar.app.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew Tap
+
+Homebrew tap for [DeployBar](https://github.com/snapre/DeployBar).
+
+```sh
+brew tap snapre/tap
+brew install --cask deploybar
+```
+
