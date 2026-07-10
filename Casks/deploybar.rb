fce926358b4d4e096f6da53b06a6649531f39f44
Xiang Wu <xiangwu619@gmail.com>
2026-07-10 06:29:44 +0000

    Update DeployBar to v0.1.26

--- Casks/deploybar.rb
+++ Casks/deploybar.rb
@@ -1,6 +1,6 @@
 cask "deploybar" do
-  version "0.1.25"
-  sha256 "a852afb4c2da1a4c7ec608dd285d012921b18f99b703196bdf12d63f155945a8"
+  version "0.1.26"
+  sha256 "54edb7e4506882c1591cf6ef9304958ff34b74b7377c69baba85648dc5b722c3"
 
   url "https://github.com/snapre/DeployBar/releases/download/v#{version}/DeployBar-#{version}-macOS.zip"
   name "DeployBar"
